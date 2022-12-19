8ad5d99ed8d3c6431f9f97cd5b291c4b124937cd
sonaliyadav13856@gmail.com <sonaliyadav13856@gmail.com>
2022-12-19 15:48:23 +0530

    new

---
+++ src/One/ONEE.java
@@ -0,0 +1,19 @@
+package One;
+
+public class ONEE {
+	  public static void  a1()
+	  {
+		  System.out.println("1st method");
+	  }
+	  public static void  a2()
+	  {
+		  System.out.println("2nd method");
+	  }
+		public static void main(String[] args) {
+			a1();
+			a2();
+
+		}
+
+	
+}
